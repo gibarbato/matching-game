5ce73131a5b22ff17d0217f1ba86d8f3b2f330ad
gilberto barbato <gibarbato@gmail.com>
2023-01-26 13:37:59 -0300

    Create CardGame component

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+function CardGame() {
+    return `
+        <article class='card-game'>
+            <img src="images/letra-g.png" alt = 'letra G'>
+        </article>
+    `
+}
+
+export default CardGame;
