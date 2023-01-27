f488ba1f86f73e5b5dfcd82a8f29e264f70e0daa
gilberto barbato <gibarbato@gmail.com>
2023-01-27 08:03:49 -0300

    Add the CardGame with javascript icon

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -4,7 +4,7 @@ function CardFrontBack() {
     return /*html*/`
         <article class='card-front-back'>
             ${CardGame()}
-            ${CardGame("css", "Logo do JavaScript")}
+            ${CardGame("javascript", "Logo do JavaScript")}
         </article>
     `;
 }
