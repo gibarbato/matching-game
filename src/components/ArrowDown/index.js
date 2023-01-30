12498d5f900b311aa3b6d757d425b0d12dbc006b
gilberto barbato <gibarbato@gmail.com>
2023-01-30 11:10:29 -0300

    Add CSS in ArrowDown with animation

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,6 +1,11 @@
-function ArrowDown(){
+import './style.css';
+
+function ArrowDown(currentPlayer = 1){
     return /*html */`
-        <img src="images/iconArrowDown.png" alt ="Ícone de uma seta para baixo">
+        <img class="arrow-down" 
+            data-currentPlayer="${currentPlayer}"
+            src="images/iconArrowDown.png" 
+            alt ="Ícone de uma seta para baixo">
     `;
 }
 
