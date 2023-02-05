05fe746bdfa4c923a7b20b391c044a61bfbf70a1
gilberto barbato <gibarbato@gmail.com>
2023-02-05 19:42:48 -0300

    Create the javascript, html and css cards

---
+++ src/objects/BoardGame/data.js
@@ -0,0 +1,22 @@
+const cards = [{
+    icon: "javascript",
+    altIcon: "Logo do JavaScript"
+},{
+    icon: "html",
+    altIcon: "Logo do HTML"
+},{
+    icon: "css",
+    altIcon: "Logo do CSS"
+},{
+    icon: "javascript",
+    altIcon: "Logo do JavaScript"
+},{
+    icon: "html",
+    altIcon: "Logo do HTML"
+},{
+    icon: "css",
+    altIcon: "Logo do CSS"
+}
+];
+
+export default cards;
